9a99d7a32f8c1fd288d88d0890047e6378f8a823
Angelica Zverovich <azverovich@stuy.edu>
2017-03-31 11:14:48 -0400

    working on it

---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,35 @@
+public class MyLinkedList {
+
+    private LNode start;
+    private int size;
+
+
+    public MyLinkedList () {
+	start = new LNode ();
+	size = 0; 
+    }
+    
+    public boolean add(int value) {
+    }
+   
+    public int size() {
+    }
+   
+    public int get(int index) {
+    } 
+  
+
+    public int set(int index,int newValue) {
+
+    }
+
+    private class LNode {
+	public  LNode next; 
+	public  int value;
+
+    }
+
+}
+
+
+} 
